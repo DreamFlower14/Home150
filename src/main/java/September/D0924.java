67d49565e1a781a2b95998ce5ca46ad31dcab886
DreamFlower14 <sunghh1004@naver.com>
2022-09-24 21:16:46 +0900

    0924 CodeUp 1734

---
+++ src/main/java/September/D0924.java
@@ -0,0 +1,13 @@
+package September;
+
+import java.util.Scanner;
+
+public class D0924 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String ID = scan.nextLine();
+
+        System.out.println("welcome! "+ID);
+    }
+}
